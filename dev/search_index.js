7fae55fd4b2d27acc2d0b42e6ba1008443db243d
Documenter.jl <documenter@juliadocs.github.io>
2024-10-18 09:02:55 +0000

    build based on 523b2be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AtomWaveguideSystems","category":"page"},{"location":"#AtomWaveguideSystems","page":"Home","title":"AtomWaveguideSystems","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AtomWaveguideSystems.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AtomWaveguideSystems]","category":"page"}]
+}
